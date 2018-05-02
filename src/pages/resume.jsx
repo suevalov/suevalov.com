bc540b567effae09b056439dba6319fad8aba46f
Alexander Suevalov <suevalov.work@gmail.com>
2018-05-02 12:50:16 +0300

    Added PDF resume and link to download it

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -45,12 +45,22 @@ const Row = styled("div")`
   }
 `;
 
+const DownloadLink = styled("a")`
+  font-size: 16px;
+  margin-left: 20px;
+`;
+
 class Resume extends React.Component {
   render() {
     return (
       <div>
         <Helmet title={Config.siteTitle} />
-        <FancyH1>Resume</FancyH1>
+        <FancyH1>
+          Resume{" "}
+          <DownloadLink href="/cv_alexander_suevalov.pdf">
+            Download as PDF
+          </DownloadLink>
+        </FancyH1>
         <Row style={{ marginTop: 50 }}>
           <div>
             <ExperienceBlock />
